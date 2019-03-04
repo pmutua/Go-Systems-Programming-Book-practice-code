af116119ca2786426153c9f2f3adaecb277a48d3
pmutua <example@live.com>
2019-03-04 12:17:17 +0300

    go data structures

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+type message struct {
+	X     int
+	Y     int
+	Label string
+}
 
 func main() {
 
@@ -67,5 +77,42 @@ func main() {
 		count++
 	}
 	fmt.Printf("anotherMap had %d elements\n", count)
+
+	// Convert Array to map
+	anArray := [4]int{1, 3, 4, 5}
+	aMap2 := make(map[string]int)
+
+	length := len(anArray)
+
+	for i := 0; i < length; i++ {
+		fmt.Printf("%s ", strconv.Itoa(i))
+		aMap2[strconv.Itoa(i)] = anArray[i]
+	}
+
+	for key, value := range aMap {
+		fmt.Printf("%s: %d\n", key, value)
+	}
+
+	// Go data structures
+
+	p1 := message{23, 12, "A message"}
+	p2 := message{}
+	p2.Label = "Message 2"
+
+	s1 := reflect.ValueOf(&p1).Elem()
+	s2 := reflect.ValueOf(&p2).Elem()
+	fmt.Println("S2= ", s2)
+
+	// Print all fields without knowing thier names
+	typeOfT := s1.Type()
+	fmt.Println("P1=", p1)
+	fmt.Println("P2=", p2)
+
+	for i := 0; i < s1.NumField(); i++ {
+		f := s1.Field(i)
+
+		fmt.Printf("%d: %s ", i, typeOfT.Field(i).Name)
+		fmt.Printf("%s = %v\n", f.Type(), f.Interface())
+	}
 
 }
