c9a9fc0872eb5c022ec67e0f5d180de67676843e
pmutua <example@live.com>
2019-03-04 12:14:11 +0300

    structures in go lang.

---
+++ dataStructures.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Structure
+func main() {
+	type message struct {
+		X     int
+		Y     int
+		Label string
+	}
+
+	// Structures
+
+	p1 := message{23, 12, "A message"}
+	p2 := message{}
+	p2.Label = "Message 2"
+
+	s1 := reflect.ValueOf(&p1).Elem()
+	s2 := reflect.ValueOf(&p2).Elem()
+	fmt.Println("S2= ", s2)
+
+	// Print all fields without knowing thier names
+	typeOfT := s1.Type()
+	fmt.Println("P1=", p1)
+	fmt.Println("P2=", p2)
+
+	for i := 0; i < s1.NumField(); i++ {
+		f := s1.Field(i)
+
+		fmt.Printf("%d: %s ", i, typeOfT.Field(i).Name)
+		fmt.Printf("%s = %v\n", f.Type(), f.Interface())
+	}
+}
