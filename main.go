13dd61fc1218310eb9c8d03e31c3c59a2b0c34fd
pmutua <example@live.com>
2019-03-04 11:50:25 +0300

    Iterating over all :key: keys of an existing Map.

--- main.go
+++ main.go
@@ -42,4 +42,14 @@ func main() {
 		fmt.Printf("The Tuesday key does not exist!\n")
 	}
 
+	// Iterating over all the Keys of an existing map
+
+	count := 0
+	for key, _ := range aMap {
+		count++
+		fmt.Printf("%s ", key)
+	}
+	fmt.Printf("\n")
+	fmt.Printf("The aMap has %d elements\n", count)
+
 }
