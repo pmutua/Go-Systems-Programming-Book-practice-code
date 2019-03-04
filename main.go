fba4d2e5b28342513aff82e2477dbb504106e7c1
pmutua <example@live.com>
2019-03-04 11:39:25 +0300

    Declaring maps

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	// Slices
 	aSlice := []int{-1, 4, 5, 7, 5, 4, 5, 6, 67}
 	fmt.Printf("Before change: ")
 	printSlice(aSlice)
@@ -17,5 +19,19 @@ func main() {
 	anotherSlice := make([]int, 4)
 	fmt.Printf("A new slice with 4 elements: ")
 	printSlice(anotherSlice)
+
+	//Maps
+	aMap := make(map[string]int)
+
+	aMap["Mon"] = 0
+	aMap["Tue"] = 1
+	aMap["Wed"] = 2
+	aMap["Thu"] = 3
+	aMap["Fri"] = 4
+	aMap["Sat"] = 5
+	aMap["Sun"] = 6
+
+	// Get Value of existing Key
+	fmt.Printf("Sunday is the %dth day of the week.\n", aMap["Sun"])
 
 }
